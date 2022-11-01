d99e2f00795ebee7a4a93da198c4d7565ce8ac46
2youyou2 <501251991@qq.com>
2022-11-01 16:33:09 +0800

    refine effect

--- extensions/pipeline/pipeline/pipeline-manager.ts
+++ extensions/pipeline/pipeline/pipeline-manager.ts
@@ -2,6 +2,7 @@ import { _decorator, renderer, rendering } from 'cc';
 import { BaseStage } from './stages/base-stage';
 import { CameraSetting } from './camera-setting';
 import { EDITOR } from 'cc/env';
+import { buildDeferred } from './test-custom';
 
 export class CustomPipelineBuilder {
     static pipelines: Map<string, BaseStage[]> = new Map
@@ -17,21 +18,23 @@ export class CustomPipelineBuilder {
     public setup (cameras: renderer.scene.Camera[], ppl: rendering.Pipeline): void {
         let excludes = [
             'scene:material-previewcamera',
-            'Scene Gizmo Camera',
-            'Editor UIGizmoCamera'
+            // 'Scene Gizmo Camera',
+            // 'Editor UIGizmoCamera'
         ]
         if (EDITOR) {
             excludes.push('Main Camera')
         }
         for (let i = 0; i < cameras.length; i++) {
             const camera = cameras[i];
-            if (camera.scene === null) {
+            if (!camera.scene) {
                 continue;
             }
-            if (excludes.includes(camera.name)) {
-                continue;
-            }
-            this.renderCamera(camera, ppl)
+            buildDeferred(camera, ppl);
+
+            // if (excludes.includes(camera.name)) {
+            //     continue;
+            // }
+            // this.renderCamera(camera, ppl)
         }
     }
     renderCamera (camera: renderer.scene.Camera, ppl: rendering.Pipeline) {
