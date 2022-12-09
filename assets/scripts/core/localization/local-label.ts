915d0ae216fa4dee36350115062c25b033867add
Canvas <canvasfull@gmail.com>
2022-12-09 16:21:13 +0800

    remove unused code & fix local-label code formatting.

--- assets/scripts/core/localization/local-label.ts
+++ assets/scripts/core/localization/local-label.ts
@@ -1,28 +1,29 @@
 import { _decorator, Component, Node, Label } from 'cc';
 import { Msg } from '../msg/msg';
+import { UtilNode } from '../util/util';
 import { Local } from './local';
 const { ccclass, property } = _decorator;
 
-@ccclass('local_label')
+@ccclass('LocalLabel')
 export class LocalLabel extends Component {
     
-    label:Label = Object.create(null);
+    label:Label | undefined;
 
     key:string = '';
 
     start() {
-        Msg.on('refresh_local',this.refresh.bind(this));
-        this.label = this.getComponent(Label);
+        Msg.bind('refresh_local',this.refresh, this);
+        this.label = UtilNode.getComponent(this.node, Label);
         this.key = this.node.name.slice(6);
         this.refresh();
     }
 
     onDestroy() {
-        Msg.off('refresh_local', this.refresh.bind)
+        Msg.off('refresh_local', this.refresh);
     }
 
     refresh() {
-        this.label.string = Local.Instance.get(this.key);
+        this.label!.string = Local.Instance.get(this.key);
     }
 
 }
