ff544d91e5d641bb7d0cfede8a6f31cfa09866ac
Canvas <canvasfull@gmail.com>
2022-12-26 18:35:53 +0800

    add grenade_0 & grenade_1 & handgun_1 res.

--- assets/scripts/core/sensor/sensor-rays-angle.ts
+++ assets/scripts/core/sensor/sensor-rays-angle.ts
@@ -30,18 +30,18 @@ export class SensorRaysAngle extends Component {
 
     checked = false;
 
-    checkedNode = null;
+    checkedNode:Node | undefined;
 
     hitPoint:Vec3 = v3(0, 0, 0);
 
     _time = 0;
 
-    _mask:number;
+    _mask = 0;
 
     _curDir = v3(0, 0, 0);
 
-    _dirs: Array<Vec3>;
-    _origins: Array<Vec3>;
+    _dirs: Array<Vec3> | undefined;
+    _origins: Array<Vec3> | undefined;
 
     start() {
         for(let i = 0; i < this.masks.length; i++)
@@ -50,14 +50,14 @@ export class SensorRaysAngle extends Component {
         this._dirs = new Array(this.dir.length * this.segment);
         this._origins = new Array(this.dir.length * this.segment);
         const each_angle = this.angle/this.segment;
-        const rotationOrignl = v3(0, 0, 0);
+        const rotationOrigins = v3(0, 0, 0);
         for(let i = 0; i < this.dir.length; i++) {
             const d = this.dir[i];
             let curAngle = -this.angle/2;
             for(let j = 0; j < this.segment; j++) {
                 curAngle += each_angle;
                 let newDir = v3(d.x, d.y, d.z);
-                Vec3.rotateY(newDir, newDir, rotationOrignl, math.toRadian(curAngle));
+                Vec3.rotateY(newDir, newDir, rotationOrigins, math.toRadian(curAngle));
                 const index = i * this.segment + j;
                 this._dirs[index] = newDir;
                 this._origins[index] = this.origin[i];
@@ -69,18 +69,16 @@ export class SensorRaysAngle extends Component {
         this._time -= deltaTime;
         if (this._time < 0) {
             this._time = this.check_time; 
-            for(let i = 0; i < this._dirs.length; i++) {
-                if (this._dirs[i] === undefined) {
-                    console.log(this._dirs[i]);
-                }
-                Vec3.transformMat4(this._curDir, this._dirs[i], this.node.worldMatrix);
+            for(let i = 0; i < this._dirs!.length; i++) {
+                const dir = this._dirs![i];
+                Vec3.transformMat4(this._curDir, dir, this.node.worldMatrix);
                 this._curDir = this._curDir.subtract(this.node.worldPosition).normalize();
                 this._ray.d.x = this._curDir.x;
                 this._ray.d.y = this._curDir.y;
                 this._ray.d.z = this._curDir.z;
-                this._ray.o.x = this._origins[i].x + this.node.worldPosition.x;
-                this._ray.o.y = this._origins[i].y + this.node.worldPosition.y;
-                this._ray.o.z = this._origins[i].z + this.node.worldPosition.z;
+                this._ray.o.x = this._origins![i].x + this.node.worldPosition.x;
+                this._ray.o.y = this._origins![i].y + this.node.worldPosition.y;
+                this._ray.o.z = this._origins![i].z + this.node.worldPosition.z;
                 if (PhysicsSystem.instance.raycastClosest(this._ray, this._mask, this.distance)) {
                     var res = PhysicsSystem.instance.raycastClosestResult;
                     u3.c(this.hitPoint, res.hitPoint);
@@ -88,7 +86,7 @@ export class SensorRaysAngle extends Component {
                     this.checkedNode = res.collider.node;
                     return;
                 }else{
-                    this.checkedNode = null;
+                    this.checkedNode = undefined;
                     this.checked = false;
                 }
             }
