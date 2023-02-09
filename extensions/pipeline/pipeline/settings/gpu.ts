cc65280df103c517085b9313090cfca01b335135
2youyou2 <501251991@qq.com>
2023-02-09 16:11:41 +0800

    refine gpu detect

--- extensions/pipeline/pipeline/settings/gpu.ts
+++ extensions/pipeline/pipeline/settings/gpu.ts
@@ -2,7 +2,9 @@ import { sys } from 'cc';
 import { JSB, MINIGAME } from 'cc/env';
 import Event from '../utils/event';
 
-import detectGPU from '../lib/detect-gpu.umd.js'
+// import detectGPU from '../lib/detect-gpu.umd.js'
+
+import * as detectGPU from '../lib/detect-gpu'
 
 export enum RenderQulity {
     Low,
@@ -22,27 +24,6 @@ export let gpuTier = {
 export let gpuTierUpdated = new Event;
 
 let inited = false;
-
-detectGPU.getGPUTier({
-    mobileTiers: [0, 60, 150, 300],
-    desktopTiers: [0, 60, 150, 300]
-}).then((tier: any) => {
-    gpuTier = tier;
-
-    for (let name in tier) {
-        gpuTier[name] = tier[name];
-    }
-
-    if (MINIGAME) {
-        gpuTier.tier = 1;
-    }
-    else if (sys.isMobile) {
-        gpuTier.tier -= 1;
-    }
-
-    inited = true;
-    gpuTierUpdated.invoke();
-});
 
 export async function waitForGpuInited () {
     if (inited) {
@@ -68,3 +49,45 @@ export function getTierName () {
 
 (window as any).getGPUTier = detectGPU.getGPUTier;
 (window as any).getTierName = getTierName;
+
+let director = cc.director
+let game = cc.game
+
+// game.on(Game.EVENT_ENGINE_INITED, () => {
+let renderer = '';
+if (director.root && director.root.device) {
+    renderer = director.root.device.renderer;
+}
+console.log('gpu : ' + renderer)
+
+detectGPU.getGPUTier({
+    // benchmarksURL: 'https://preview.cocos.com/cyberpunk/',
+    mobileTiers: [0, 60, 150, 300],
+    desktopTiers: [0, 60, 150, 300],
+    override: {
+        renderer,
+        isMobile: sys.isMobile,
+        screenSize: {
+            width: game.canvas.width,
+            height: game.canvas.height
+        }
+    }
+}).then((tier: any) => {
+    gpuTier = tier;
+
+    for (let name in tier) {
+        gpuTier[name] = tier[name];
+    }
+
+    if (MINIGAME) {
+        gpuTier.tier = 1;
+    }
+    else if (sys.isMobile) {
+        gpuTier.tier -= 1;
+    }
+
+    inited = true;
+    gpuTierUpdated.invoke();
+});
+
+// })
